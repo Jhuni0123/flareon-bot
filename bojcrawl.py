06cd9c708222a3e7999175d676f1669b4168fbdc
Jhuni0123 <whdgnsdl887@naver.com>
2016-12-30 19:00:42 +0900

    make boj crawler

--- bojcrawl.py
+++ bojcrawl.py
@@ -2,6 +2,35 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+class BOJCrawler:
+    address = 'https://www.acmicpc.net/'
+    short_address = 'https://boj.kr/'
+    def get_problem_title(self, num):
+        try:
+            plainCode = request.get(address + 'problem/' + str(num), timeout = 5)
+            soup = BeautifulSoup(plainCode.text, 'html.parser')
+            title = soup.select('head > title')[0].string
+            if title == 'Baekjoon Online Judge':
+                return False
+            else:
+                return title
+        except requests.Timeout:
+            raise
+        except:
+            return False
+
+    def command(self, text):
+        if text.isnumeric():
+            num = int(text)
+            try:
+                title = self.get_problem_title(num)
+            except requests.Timeout:
+                return ['Timeout']
+            if title:
+                return ['%s - %s/%d' % (title, short_address, num)]
+            else:
+                return ['Problem not found']
+
 def BOJCrawl(url):
     try:
         plainCode = requests.get(url,timeout=None)
